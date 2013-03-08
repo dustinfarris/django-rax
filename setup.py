e8d0ba034cca6a4b7e1d15cbcf68352fa47c4bbe
Dustin Farris <dustin@dustinfarris.com>
2013-03-08 02:41:02 -0500

    Version 0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='django-rax',
-    version='0.2',
+    version='0.3',
     author='Dustin Farris',
     author_email='dustin@dustinfarris.com',
     url='https://github.com/dustinfarris/django-rax',
